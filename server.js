697de2cecc1d318e1050b8284158956ab75cde20
Sakshi <ghera.sakshi@gmail.com>
2024-03-11 16:47:02 -0400

    Added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path'); // Add this line
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define the route for serving the index.html file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
