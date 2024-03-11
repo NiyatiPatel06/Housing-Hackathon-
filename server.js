0b3eb2d80688276e69a0cf9f611c24328b0d93ed
Niyati Patel <npatek413@myseneca.ca>
2024-03-11 17:24:53 -0400

    connecting oages

--- server.js
+++ server.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/addListing.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'addListing.html'));
+});
+
+app.get('/findRoommate.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'findRoommate.html'));
+});
+
 // Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
