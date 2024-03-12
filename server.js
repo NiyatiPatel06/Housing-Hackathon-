eab26972ecaa254c131de54194f3d2b46de38417
Sakshi <ghera.sakshi@gmail.com>
2024-03-11 20:11:38 -0400

    Final MVP

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-
 // Start the server
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
